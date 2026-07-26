bf4d8ee3d8b5198cb7f183b832d1778986dfec3b
mezoo2003 <nursing2112003@gmail.com>
2026-07-26 23:03:14 +0300

    Update gallery_tab.dart

--- lib/screens/gallery/gallery_tab.dart
+++ lib/screens/gallery/gallery_tab.dart
@@ -6,6 +6,7 @@ import '../../core/theme/app_colors.dart';
 import '../../l10n/app_strings.dart';
 import '../../providers/gallery_provider.dart';
 import '../../providers/hashtag_provider.dart';
+import '../../providers/settings_provider.dart';
 import '../../widgets/add_hashtag_sheet.dart';
 import '../../widgets/empty_state.dart';
 import '../../widgets/photo_thumb.dart';
@@ -63,8 +64,8 @@ class _GalleryTabState extends State<GalleryTab> {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer2<GalleryProvider, HashtagProvider>(
-      builder: (context, gallery, hashtagP, _) {
+    return Consumer3<GalleryProvider, HashtagProvider, SettingsProvider>(
+      builder: (context, gallery, hashtagP, settings, _) {
         Widget body;
 
         if (gallery.status == GalleryStatus.initial || gallery.status == GalleryStatus.loading) {
@@ -103,11 +104,118 @@ class _GalleryTabState extends State<GalleryTab> {
             WidgetsBinding.instance.addPostFrameCallback((_) => _loadTagMap(gallery.assets));
           }
 
-          final visible = gallery.assets.where((a) => !_isHiddenByLock(a, hashtagP)).toList();
+          final visible = gallery.assets.where((a) => !_isHiddenByLock(a, hashtagP)).toList()
+            ..sort((a, b) => settings.sortNewestFirst
+                ? b.createDateTime.compareTo(a.createDateTime)
+                : a.createDateTime.compareTo(b.createDateTime));
 
           body = RefreshIndicator(
             onRefresh: gallery.refresh,
             child: GridView.builder(
+              padding: const EdgeInsets.all(2),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 5,
+                crossAxisSpacing: 2,
+                mainAxisSpacing: 2,
+              ),
+              itemCount: visible.length,
+              itemBuilder: (context, i) {
+                final asset = visible[i];
+                final selected = _selectedIds.contains(asset.id);
+                return PhotoThumb(
+                  asset: asset,
+                  selectionMode: _selectionMode,
+                  selected: selected,
+                  onTap: () {
+                    if (_selectionMode) {
+                      _toggleSelect(asset.id);
+                    } else {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => PhotoDetailScreen(assets: visible, initialIndex: i),
+                        ),
+                      );
+                    }
+                  },
+                  onLongPress: () {
+                    if (_selectionMode) return;
+                    showAddHashtagSheet(context, assetIds: [asset.id]);
+                  },
+                );
+              },
+            ),
+          );
+        }
+
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(context.tr('gallery')),
+            actions: [
+              if (gallery.status == GalleryStatus.ready &&
+                  gallery.assets.isNotEmpty &&
+                  !_selectionMode)
+                IconButton(
+                  tooltip: settings.sortNewestFirst
+                      ? context.tr('sortOldestFirst')
+                      : context.tr('sortNewestFirst'),
+                  icon: Icon(
+                    settings.sortNewestFirst ? Icons.arrow_downward : Icons.arrow_upward,
+                  ),
+                  onPressed: settings.toggleSortOrder,
+                ),
+              if (gallery.status == GalleryStatus.ready && gallery.assets.isNotEmpty)
+                IconButton(
+                  tooltip: context.tr('selectAll'),
+                  icon: Icon(_selectionMode ? Icons.close : Icons.checklist_rounded),
+                  onPressed: () {
+                    if (_selectionMode) {
+                      _exitSelection();
+                    } else {
+                      setState(() => _selectionMode = true);
+                    }
+                  },
+                ),
+            ],
+          ),
+          body: Column(
+            children: [
+              if (_selectionMode)
+                Container(
+                  width: double.infinity,
+                  color: AppColors.primary.withOpacity(0.08),
+                  padding: const EdgeInsets.symmetric(vertical: 6),
+                  child: Text(
+                    context.tr('bulkSelectHint'),
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(color: AppColors.primary, fontSize: 12.5),
+                  ),
+                ),
+              Expanded(child: body),
+            ],
+          ),
+          bottomNavigationBar: _selectionMode
+              ? SelectionToolbar(
+                  selectedCount: _selectedIds.length,
+                  onSelectAll: () => setState(
+                    () => _selectedIds.addAll(gallery.assets.map((a) => a.id)),
+                  ),
+                  onAddHashtag: () async {
+                    await showAddHashtagSheet(
+                      context,
+                      assetIds: _selectedIds.toList(),
+                      bulk: true,
+                    );
+                    _exitSelection();
+                  },
+                  onCancel: _exitSelection,
+                )
+              : null,
+        );
+      },
+    );
+  }
+}            child: GridView.builder(
               padding: const EdgeInsets.all(2),
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 3,
